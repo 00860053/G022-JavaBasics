659e75ca0a938ee8cbbe5ad6640a7624fc6e7aa9
00860053 <00860053@edu.campus02.at>
2022-09-23 17:10:22 +0200

    My first Class created; args as input parameter written to console

---
+++ MyFirstClass.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class MyFirstClass {
+    //wir haben unsere erste Klasse erzeugt
+
+    //psvm
+    public static void main(String[] args) {
+        // erste Ausgabe
+        System.out.println("Ich werde in der Konsole angezeigt");
+
+        //args
+        System.out.println(Arrays.toString(args));
+    }
+}
